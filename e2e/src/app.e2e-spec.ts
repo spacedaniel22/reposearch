ab6c201606ee8a6e7cece7ea51b24bbfd22d94c8
Daniel Lorincz <daniel.lorincz@betssongroup.com>
2018-05-16 09:17:21 +0200

    fix tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,15 @@
-import { AppPage } from './app.po';
+import { AppPage } from "./app.po";
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+describe("workspace-project App", () => {
+    let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+        page = new AppPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+    it("should display welcome message", () => {
+        const title = "Repo Search";
+        page.navigateTo();
+        expect(page.getParagraphText()).toEqual(`Welcome to ${title}!`);
+    });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,23 @@
 import { TestBed, async } from "@angular/core/testing";
+import { APP_BASE_HREF } from "@angular/common";
+
 import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { RepoSearchModule } from "./repo-search/repo-search.module";
+import { IssuesModule } from "./issues/issues-module";
 describe("AppComponent", () => {
+    const title = "Repo Search";
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
                 AppComponent
             ],
+            providers: [{provide: APP_BASE_HREF, useValue : "/" }],
+            imports: [
+                AppRoutingModule,
+                RepoSearchModule,
+                IssuesModule
+            ]
         }).compileComponents();
     }));
     it("should create the app", async(() => {
@@ -13,15 +25,15 @@ describe("AppComponent", () => {
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
     }));
-    it(`should have as title 'Repo Search'`, async(() => {
+    it(`should have as title '${title}'`, async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual("Repo Search");
+        expect(app.title).toEqual(title);
     }));
     it("should render title in a h1 tag", async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector("h1").textContent).toContain("Welcome to app!");
+        expect(compiled.querySelector("h1").textContent).toContain(`Welcome to ${title}!`);
     }));
 });
